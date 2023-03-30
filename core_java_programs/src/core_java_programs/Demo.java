055711487da522dab101437572304bd6b34952fa
Kruthika M <Kruthika M@SMTC-R-0151>
2023-03-30 10:33:40 +0530

    project pushed on 30/03/2023

---
+++ core_java_programs/src/core_java_programs/Demo.java
@@ -0,0 +1,7 @@
+package core_java_programs;
+
+public class Demo {
+public static void main(String[] args) {
+	System.out.println("hello world");
+}
+}
---
+++ core_java_programs/src/pattern/P1.java
@@ -0,0 +1,7 @@
+package pattern;
+
+public class P1 {
+public static void main(String[] args) {
+	System.out.println("hello kruhtika");
+}
+}
